6b5817909eac4d8433f60103c43fdc7e23b1af4d
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-03-29 19:05:03 +0300

    main
    Signed-off-by: FIRST_NAME LAST_NAME <MY_NAME@example.com>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import Stars from './task1/Stars';
+import Listing from './task2/Listing';
+import data from './task2/data';
+import MessageHistory from './task3/MessageHistory';
+import messages from './task3/data/messages';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> 
+      <div className="task-1">  
+        <Stars count={1} />
+        <Stars count={2} />
+        <Stars count={3} />
+        <Stars count={4} />
+        <Stars count={5} />
+      </div>
+      <div className="task-2">
+        <Listing items={data} />
+      </div>
+      <div className="task-3">
+        <div className="clearfix container">
+          <div className="chat">
+            <div className="chat-history">
+              <MessageHistory list={messages} />
+            </div>
+          </div>  
+        </div>
+      </div>
     </div>
   );
 }
